125e4b595b4add8aea401f0f9b57ef059a41ef31
Bielbr123 <gabriellyra46@gmail.com>
2026-01-21 18:12:27 -0300

    Chore: comitando para poder deixar tudo em dia entre aqui e o online'

--- class_probability_estimation/src/importData.py
+++ class_probability_estimation/src/importData.py
@@ -1,9 +1,6 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
 import pandas as pd
-import pathlib
 
-#workingPath = pathlib.Path(__file__).parent.resolve()
-#dataPath = f'{workingPath}/dados_clients.csv' 
-#print(dataPath)
 # Venv instalou o Pandas no repositório "class_probability-estimation"!
 csvDf = pd.read_csv('src/dados_clientes.csv')
-print(csvDf.to_string())
