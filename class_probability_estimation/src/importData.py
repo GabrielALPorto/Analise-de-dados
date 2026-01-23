244ca0c8782040f8f21243b09f750c6509cb7886
Bielbr123 <gabriellyra46@gmail.com>
2026-01-23 01:13:05 -0300

    Chore: removendo a pasta class_probability_estimation do GitHub temporariamente

--- class_probability_estimation/src/importData.py
+++
@@ -1,6 +0,0 @@
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import make_classification
-import pandas as pd
-
-# Venv instalou o Pandas no repositório "class_probability-estimation"!
-csvDf = pd.read_csv('src/dados_clientes.csv')
